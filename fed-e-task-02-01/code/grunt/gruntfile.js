c639264553482878ca6833085e77d3c138999767
lcj <947993447@qq.com>
2020-06-15 20:16:52 +0800

    lcj_task

--- fed-e-task-02-01/code/grunt/gruntfile.js
+++ fed-e-task-02-01/code/grunt/gruntfile.js
@@ -1,16 +1,58 @@
 module.exports = grunt => {
   // grunt.initConfig() 用于为任务添加一些配置选项
   grunt.initConfig({
-    
-    foo: {
-      bar: 'baz'
+    menus: [
+      {
+        name: 'Home',
+        icon: 'aperture',
+        link: 'index.html'
+      },
+      {
+        name: 'Features',
+        link: 'features.html'
+      },
+      {
+        name: 'About',
+        link: 'about.html'
+      },
+      {
+        name: 'Contact',
+        link: '#',
+        children: [
+          {
+            name: 'Twitter',
+            link: 'https://twitter.com/w_zce'
+          },
+          {
+            name: 'About',
+            link: 'https://weibo.com/zceme'
+          },
+          {
+            name: 'divider'
+          },
+          {
+            name: 'About',
+            link: 'https://github.com/zce'
+          }
+        ]
+      }
+    ],
+    pkg: require('./package.json'),
+    date: new Date(),
+    clean: {
+      src: ['build/', 'temp/']
+    },
+
+    lint: {
+
     }
+
   })
 
-  grunt.registerTask('foo', () => {
-    // 任务中可以使用 grunt.config() 获取配置
-    console.log(grunt.config('foo'))
-    // 如果属性值是对象的话，config 中可以使用点的方式定位对象中属性的值
-    console.log(grunt.config('foo.bar'))
-  })
+  const browserSync = require('browser-sync')
+  const bs = browserSync.create()
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  
 }

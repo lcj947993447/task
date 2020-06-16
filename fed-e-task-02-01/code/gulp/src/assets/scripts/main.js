ec2ec813bc8b91df4b899b7142be67bbed381485
liang1996 <947993447@qq.com>
2020-06-16 23:21:45 +0800

    lcj_task

--- fed-e-task-02-01/code/gulp/src/assets/scripts/main.js
+++ fed-e-task-02-01/code/gulp/src/assets/scripts/main.js
@@ -2,7 +2,11 @@
 
 $(($) => {
   const $body = $('html, body')
+  console.log("123")
 
+  // let data = {
+  //   name: 'lcj'
+  // }
   $('#scroll_top').on('click', () => {
     $body.animate({ scrollTop: 0 }, 600)
     return false

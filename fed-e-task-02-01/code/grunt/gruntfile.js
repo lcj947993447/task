ab9e68f12529933be2d72c331ae2ca8ac3a3f7b6
lcj <947993447@qq.com>
2020-06-16 11:58:04 +0800

    lcj_task

--- fed-e-task-02-01/code/grunt/gruntfile.js
+++ fed-e-task-02-01/code/grunt/gruntfile.js
@@ -40,19 +40,60 @@ module.exports = grunt => {
     pkg: require('./package.json'),
     date: new Date(),
     clean: {
-      src: ['build/', 'temp/']
+      src: ['dist/', 'temp/']
     },
 
-    lint: {
+    jshint: {
+      all: [
+        'src/assets/scripts/*.js'
+      ],
+      jshintrc: true,
+      options: {
+        browser: true,
+        devel: true
+      }
+    },
 
+    scsslint: {
+      allFiles: [
+        'src/assets/styles/*.scss'
+      ],
+      options: {
+        bundleExec: true,
+        config: '.scss-lint.yml',
+        reporterOutput: 'scss-lint-report.xml',
+        colorizeOutput: true
+      }
     }
 
   })
 
-  const browserSync = require('browser-sync')
-  const bs = browserSync.create()
+  // 删除
+  grunt.loadNpmTasks('grunt-contrib-clean')
+  // js css语法检查
+  grunt.loadNpmTasks('grunt-contrib-jshint')
+  grunt.loadNpmTasks('grunt-scss-lint')
+  // 监听
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  // sass编译
+  grunt.loadNpmTasks('grunt-contrib-sass')
+  // js css img html压缩
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-contrib-cssmin')
+  grunt.loadNpmTasks('grunt-contrib-imagemin')
+  grunt.loadNpmTasks('grunt-contrib-htmlmin')
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  
+
+
+  grunt.registerTask('default', ['clean'])
+  grunt.registerTask('lint', 'jshint and scsslint', () => {
+    grunt.task.run('scsslint', 'jshint')
+  })
+  // grunt.registerTask('default', ['sass', 'babel', 'watch'])
+
+  // const browserSync = require('browser-sync')
+  // const bs = browserSync.create()
+
+
+
 }
--- fed-e-task-02-01/code/grunt/src/assets/scripts/main.js
+++ fed-e-task-02-01/code/grunt/src/assets/scripts/main.js
@@ -1,8 +1,8 @@
 // TODO: site logics
 
 $(($) => {
-  const $body = $('html, body')
-
+  const $body = $('html, body');;;
+  123
   $('#scroll_top').on('click', () => {
     $body.animate({ scrollTop: 0 }, 600)
     return false

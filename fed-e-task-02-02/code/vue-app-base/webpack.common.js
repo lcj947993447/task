b55856e770a4a5ac97eef5133cec42fce398d511
lcj <947993447@qq.com>
2020-06-27 11:35:03 +0800

    lcj_task

--- fed-e-task-02-02/code/vue-app-base/webpack.common.js
+++ fed-e-task-02-02/code/vue-app-base/webpack.common.js
@@ -3,70 +3,79 @@ const webpack = require('webpack')
 const VueLoaderPlugin = require('vue-loader/lib/plugin') // vue-loader 插件，它的职责是将你定义过的其它规则复制并应用到 .vue 文件里相应语言的块
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-	entry: './src/main.js',
-	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				options: {
-					presets: ['@babel/preset-env'],
-				},
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: ['vue-style-loader', 'css-loader'],
-			},
-			{
-				test: /\.less$/,
-				use: ['vue-style-loader', 'css-loader', 'less-loader'],
-			},
-			{
-				test: /\.(png|jpe?g|gif)$/,
-				use: {
-					loader: 'file-loader',
+module.exports = (env, argv) => {
+	const config = {
+		target: 'web',
+		entry: path.resolve(__dirname, './src/js/main.js'),//指定打包文件
+		output: {
+			filename: '[name].js',
+			path: path.resolve(__dirname, 'dist'),
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					loader: 'babel-loader',
 					options: {
-						outputPath: 'img',
-						name: '[name].[ext]',
+						presets: ['@babel/preset-env'],
+					},
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.css$/,
+					use: ['vue-style-loader', 'css-loader'],
+				},
+				{
+					test: /\.less$/,
+					use: ['vue-style-loader', 'css-loader', 'less-loader'],
+				},
+				{
+					test: /\.(png|jpe?g|gif)$/,
+					use: {
+						loader: 'url-loader',
+						options: {
+							options: {
+								limit: 1024, // 判断图片的大小   如果小于1024就会转换成base64
+								name: '[name].[ext]' // 输出图片的名字  ext是扩展名
+							}
+						},
 					},
 				},
-			},
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-				options: {
-					hotReload: true, // disables Hot Reload
+				{
+					test: /\.vue$/,
+					loader: 'vue-loader',
+					options: {
+						hotReload: true, // disables Hot Reload
+					},
 				},
-			},
-		],
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			// 值要求的是一个代码片段
-			BASE_URL: JSON.stringify('../public/'),
-		}),
-		// 用于生成 index.html
-		new HtmlWebpackPlugin({
-			title: 'Webpack Vue',
-			meta: {
-				viewport: 'width=device-width',
-			},
-			template: './public/index.html',
-			filename: 'index.html',
-		}),
-		new VueLoaderPlugin(),
-	],
-	resolve: {
-		// ** 在.vue文件中引入.vue文件时需要更改vue文件指向 ** 说明：node_modules/vue/dist/README.md
-		alias: {
-			vue$: 'vue/dist/vue.esm.js',
-			'@': 'src',
+			],
 		},
-	},
+		plugins: [
+			new webpack.DefinePlugin({
+				// 值要求的是一个代码片段
+				BASE_URL: JSON.stringify('../public/'),
+			}),
+			// 用于生成 index.html
+			new HtmlWebpackPlugin({
+				title: 'Webpack Vue',
+				meta: {
+					viewport: 'width=device-width',
+				},
+				template: path.resolve(__dirname, 'public/index.html'), //以当前目录下的index.html文件为模板生成dist/index.html文件
+			}),
+			new VueLoaderPlugin(),
+		],
+		resolve: { // 用来配置.vue文件内引用组件的处理
+			extensions: [
+				'.vue', '.js'
+			],
+			modules: ["node_modules"],
+			alias: {
+				vue: 'vue/dist/vue.min.js',
+				'@': path.join(__dirname, '.', 'src'),
+				'@components': path.join(__dirname, '.', 'src/components')
+			}
+		},
+	}
+	return config
 }
--- fed-e-task-02-02/code/vue-app-base/webpack.dev.js
+++ fed-e-task-02-02/code/vue-app-base/webpack.dev.js
@@ -1,18 +1,18 @@
+const path = require('path')
 const webpack = require("webpack");
 const merge = require("webpack-merge");
-const common = require("./webpack.common");
-const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "development",
   devServer: {
-	contentBase: "dist",
+    contentBase: path.join(__dirname, 'dist'),
     hot: true,
     open: true,
+    port: 8080,
   },
   devtool: "cheap-eval-module-source-map", // 错误追踪
   plugins: [
-	new webpack.NamedModulesPlugin(),
-	new webpack.HotModuleReplacementPlugin(), // 热替换
+    new webpack.HotModuleReplacementPlugin(), // 热替换
   ],
 });
